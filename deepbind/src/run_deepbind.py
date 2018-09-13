3389fcdaa54098ed43ea598f3498be03d9985666
Lee Zamparo <zamparo@gmail.com>
2018-09-13 15:49:38 -0400

    added updated kipoi model execution script

--- deepbind/src/run_deepbind.py
+++ deepbind/src/run_deepbind.py
@@ -2,7 +2,8 @@ import kipoi
 import os
 import sys
 import time
-
+from dataloader import TruncSeqDataset as DL
+import pdb
 
 def process_id(line):
 	line = line.strip()
@@ -27,7 +28,7 @@ data_prefix = "~/projects/revisions/deepbind/data"
 output_prefix = "~/projects/revisions/deepbind/outputs"
 
 k562_atlas_bed = os.path.join(os.path.expanduser(data_prefix), "K562_atac.bed")
-k562_atlas_fasta = os.path.join(os.path.expanduser(data_prefix), "atac.fasta")
+k562_atlas_fasta = os.path.join(os.path.expanduser(data_prefix), "fixed_atac.fasta")
 
 model_id = model_prefix + process_id(tf_line)
 outfile_name = generate_output_file(tf_line)
@@ -46,10 +47,14 @@ print("Loaded the model", flush=True)
 dl_kwargs = {'intervals_file': k562_atlas_bed, 'fasta_file': k562_atlas_fasta}
 
 # Option 1: use data loader instantiation of an iterator directly
-dl = model.default_dataloader(**dl_kwargs)
+dl = DL(**dl_kwargs)
+
+#dl = model.default_dataloader(**dl_kwargs)
 
 t0 = time.time()
-with dl.batch_iter(batch_size=128) as batch_iter, open(output_file, 'w') as outfile:
+with open(output_file, 'w') as outfile:
+	pdb.set_trace()
+	batch_iter = dl.batch_iter(batch_size=128)
 	for batch in batch_iter:
 		preds = model.predict_on_batch(batch['inputs'])
 		try:
@@ -59,11 +64,30 @@ with dl.batch_iter(batch_size=128) as batch_iter, open(output_file, 'w') as outf
 		except:
 			print("Cannot iterate through preds in the naive way")
 t1 = time.time()
+
+#with dl.batch_iter(batch_size=128) as batch_iter, open(output_file, 'w') as outfile:
+#	for batch in batch_iter:
+#		preds = model.predict_on_batch(batch['inputs'])
+#		try:
+#			print("preds returns ", type(preds), flush=True)
+#			for p in preds:
+#				print(p, file=outfile)
+#		except:
+#			print("Cannot iterate through preds in the naive way")
+
 print("Took ", t1 - t0, "s to complete", flush=True)
 
 # Option 2: use pipeline.predict passing the dl_kwargs dict directly 
 # for prediction on all sites in the atlas in stages  
-#preds_on_atlas = model.pipeline.predict(dl_kwargs, batch_size=128)
+
+#t0 = time.time()
+#try:
+#	preds_on_atlas = model.pipeline.predict(dl_kwargs, batch_size=128)
+#except AssertionError as ae:
+#	print("Assertion error of some kind:", ae)
+#t1 = time.time()
+#print("Took ", t1 - t0, "s to complete", flush=True)
+
 #with open(outfile_name, 'w') as outfile:
 #	for p in preds_on_atlas:
 #		print(p, file=outfile)
